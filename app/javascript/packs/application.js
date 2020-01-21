d8bd39f00d13d0d7e38e03ee3dc6f295bbe23314
Hirokazu-Okazaki <gm.okazaki@gmail.com>
2020-01-21 15:15:47 +0000

    JSファイルとCSSファイルをWebpacker用に修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import '../stylesheets/application.css'
+import '../stylesheets/scaffolds.css'
+import '../stylesheets/users.css'
