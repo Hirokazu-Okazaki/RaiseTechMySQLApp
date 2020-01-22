aa406f09992584693fba09247024d97faa37e245
Hirokazu-Okazaki <gm.okazaki@gmail.com>
2020-01-22 18:19:07 +0000

    production環境構築

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ ARG RUBY_VERSION
 FROM ruby:$RUBY_VERSION
 
 ARG APP_PATH
+ARG RAILS_ENV
+ARG RACK_ENV
 ENV APP_PATH=$APP_PATH
+ENV RAILS_ENV=$RAILS_ENV
+ENV RACK_ENV=$RACK_ENV
 ENV LANG ja_JP.UTF-8
 
 # 依存関係をインストール
@@ -28,12 +32,21 @@ COPY Gemfile Gemfile.lock ./
 RUN gem update bundler
 # BundlerでGemをインストール
 RUN bundle install
+
 # カレントディレクトリの内容をコンテナへコピー
 COPY . .
 # yarnをインストール
 RUN yarn install --check-files
 
-# Add a script to be executed every time the container starts.
+# productionの設定
+RUN if [ "${RAILS_ENV}" = "production" ]; then \
+    # puma.sockを配置するディレクトリを作成
+    mkdir -p tmp/sockets; \
+    # productionなのでPrecompileする(不要?)
+    #bundle exec rails assets:precompile; \
+fi
+
+# entrypoint
 COPY entrypoint.sh /usr/bin/
 RUN chmod +x /usr/bin/entrypoint.sh
 ENTRYPOINT ["entrypoint.sh"]
--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,11 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
+if rails_env == "development"
+    # 開発環境ではNginxを使用せず3000番ポートで起動
+    port        ENV.fetch("PORT") { 3000 }    
+end
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -36,3 +40,11 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if rails_env == "production"
+    # 本番環境ではNginxを使用
+    app_root = File.expand_path("../..", __FILE__)
+    bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+    stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+end
